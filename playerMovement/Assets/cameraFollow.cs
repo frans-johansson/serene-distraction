665e57ed0e0dadf5f716b13488cc65a8cea98d6a
Astrid Johansson <astjo525@student.liu.se>
2019-03-26 11:59:29 +0100

    Projekt för att ha en karaktär som kan röra sig och en kamera som ka nröra sig

---
+++ playerMovement/Assets/cameraFollow.cs
@@ -0,0 +1,102 @@
+﻿
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    [SerializeField] private string mouseXInput = "Mouse X";
+    [SerializeField] private string mouseYInput = "Mouse Y";
+    [SerializeField] private float mouseSensitivity = 150;
+
+    private float xAxisClamp;
+
+    private void Start()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInput) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInput) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(-transform.right * mouseY);
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+
+    /*
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+    private Vector2 md; //Mouse Direction
+
+    private void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+    private void Update()
+    {
+        md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+    }*/
+
+
+
+    /*
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition= target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+    */
+}
---
+++ playerMovement/Assets/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    public float rotationRate = 360;
+
+    public float moveSpeed = 10;
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    private void Start () {
+ 
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float turnAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, turnAxis);
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+
+    private void ApplyInput(float moveInput, float turnInput)
+    {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+
+    private void Move(float input)
+    {
+        rb.AddForce(transform.forward * input * moveSpeed, ForceMode.Force);
+    }
+
+    private void Turn(float input)
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
