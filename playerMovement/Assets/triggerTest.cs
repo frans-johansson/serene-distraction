c9fdb75a97f432bf2283878f23e4b3d600f4521c
Matilda Eriksson <mater436@student.liu.se>
2019-04-02 11:56:14 +0200

    Testade att skapa en triggerpoint, när du går dit skapas en sfär

---
+++ playerMovement/Assets/triggerTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerTest : MonoBehaviour {
+
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    private bool isSphere = false;
+
+	// Use this for initialization
+	void Start () {
+        var boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            if (!isSphere)
+            {
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.gameObject.transform.position = new Vector3(15, 5.5f, 12);
+                isSphere = true;
+            }
+        }
+    }
+
+    // stayCount allows the OnTriggerStay to be displayed less often
+    // than it actually occurs.
+    //private float stayCount = 0.0f;
+    private void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            // Nothing yet
+
+            /*if (stayCount > 0.25f)
+            {
+                Debug.Log("staying");
+                stayCount = stayCount - 0.25f;
+            }
+            else
+            {
+                stayCount = stayCount + Time.deltaTime;
+            }*/
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            // Nothing right now
+        }
+    }
+}
