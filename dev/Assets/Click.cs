867540600fd0aabad42689c7cdbde680d8154690
Astrid Johansson <astjo525@student.liu.se>
2019-04-15 16:09:06 +0200

    Fixat att objekt dyker upp när spelaren är i en trigger zone och försvinner när spelaren går ut ur zonen. Objektet är även klickbart när spelaren är i zonen

---
+++ dev/Assets/Click.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+    GameObject sphere;
+
+    void Start()
+    {
+        sphere = GameObject.Find("TriggerTest");
+    }
+
+
+    private void Update()
+    {
+
+
+    }
+    //Detect if a click occurs
+    public void OnMouseDown()
+    {
+        if (sphere.activeSelf)
+        {
+            //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+            Debug.Log("Active");
+        }
+        else
+        {
+            Debug.Log("Unactive");
+        }
+
+    }
+}
