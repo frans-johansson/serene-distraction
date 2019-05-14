7550cd0020d9f05ae04623914b9e159156670aa7
Marit Faxén <marfa110@student.liu.se>
2019-05-14 16:31:30 +0200

    stuff post process kind of

---
+++ dev/Assets/Scripts/Puzzle/PuzzleInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleInitializer : MonoBehaviour
+{
+    protected Color initialColor = new Color(0.945f, 0.176f, 0.157f, 1);
+    protected Color clickedColor = Color.white;
+
+    protected CorrectClick firstCorrect;
+    protected CorrectClick secondCorrect;
+
+    // Use this for initialization
+    void Awake()
+    {
+        firstCorrect = GameObject.Find("Runa1").GetComponent<CorrectClick>();
+        secondCorrect = GameObject.Find("Runa5").GetComponent<CorrectClick>();
+    }
+}
+
